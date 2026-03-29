6494019a6861c7426611f7edba7379b4068c3988
maribelmiranda <93798520+maribelmiranda@users.noreply.github.com>
2026-03-28 19:08:14 -0600

    Add files via upload
    Añadir archivo para pruebas del curso webmapping

---
+++ escuelas.js
@@ -0,0 +1,32 @@
+const escuelas = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+
+"features": [
+{ "type": "Feature", "properties": { "Id": 1, "Nombre": "Preparatoria No 1", "Descripcion": "Gabino Barreda", "Nivel": "Bachillerato", "Poblacion": 4435 }, "geometry": { "type": "Point", "coordinates": [ -99.121852731407046, 19.2714352 ] } },
+{ "type": "Feature", "properties": { "Id": 2, "Nombre": "Preparatoria No 2", "Descripcion": "Erasmo Castellanos Quinto", "Nivel": "Bachillerato", "Poblacion": 4452 }, "geometry": { "type": "Point", "coordinates": [ -99.100176600000012, 19.384294 ] } },
+{ "type": "Feature", "properties": { "Id": 3, "Nombre": "Preparatoria No 3", "Descripcion": "Justo Sierra", "Nivel": "Bachillerato", "Poblacion": 4397 }, "geometry": { "type": "Point", "coordinates": [ -99.095184392096698, 19.4829368 ] } },
+{ "type": "Feature", "properties": { "Id": 4, "Nombre": "Preparatoria No 4", "Descripcion": "Vidal Casta�eda y N�jera", "Nivel": "Bachillerato", "Poblacion": 4719 }, "geometry": { "type": "Point", "coordinates": [ -99.195696, 19.4038285 ] } },
+{ "type": "Feature", "properties": { "Id": 5, "Nombre": "Preparatoria No 5", "Descripcion": "Jos� Vasconcelos", "Nivel": "Bachillerato", "Poblacion": 8577 }, "geometry": { "type": "Point", "coordinates": [ -99.133938199999974, 19.3077788 ] } },
+{ "type": "Feature", "properties": { "Id": 6, "Nombre": "Preparatoria No 6", "Descripcion": "Antonio Caso", "Nivel": "Bachillerato", "Poblacion": 5202 }, "geometry": { "type": "Point", "coordinates": [ -99.156027482247069, 19.35214309 ] } },
+{ "type": "Feature", "properties": { "Id": 7, "Nombre": "Preparatoria No 7", "Descripcion": "Ezequiel A. Ch�vez", "Nivel": "Bachillerato", "Poblacion": 5250 }, "geometry": { "type": "Point", "coordinates": [ -99.126635501228122, 19.4195654 ] } },
+{ "type": "Feature", "properties": { "Id": 8, "Nombre": "Preparatoria No 8", "Descripcion": "Miguel E. Schulz", "Nivel": "Bachillerato", "Poblacion": 5583 }, "geometry": { "type": "Point", "coordinates": [ -99.195133557459059, 19.36706522 ] } },
+{ "type": "Feature", "properties": { "Id": 9, "Nombre": "Preparatoria No 9", "Descripcion": "Pedro de Alba", "Nivel": "Bachillerato", "Poblacion": 4821 }, "geometry": { "type": "Point", "coordinates": [ -99.128209099999978, 19.4836888 ] } },
+{ "type": "Feature", "properties": { "Id": 10, "Nombre": "CCH Azcapotzalco", "Descripcion": null, "Nivel": "Bachillerato", "Poblacion": 11077 }, "geometry": { "type": "Point", "coordinates": [ -99.1992551, 19.3119762 ] } },
+{ "type": "Feature", "properties": { "Id": 11, "Nombre": "CCH Naucalpan", "Descripcion": null, "Nivel": "Bachillerato", "Poblacion": 11118 }, "geometry": { "type": "Point", "coordinates": [ -99.141194534326246, 19.48412912 ] } },
+{ "type": "Feature", "properties": { "Id": 12, "Nombre": "CCH Oriente", "Descripcion": null, "Nivel": "Bachillerato", "Poblacion": 11963 }, "geometry": { "type": "Point", "coordinates": [ -99.058555699999943, 19.3843969 ] } },
+{ "type": "Feature", "properties": { "Id": 13, "Nombre": "CCH Sur", "Descripcion": null, "Nivel": "Bachillerato", "Poblacion": 11421 }, "geometry": { "type": "Point", "coordinates": [ -99.244065799999959, 19.4744193 ] } },
+{ "type": "Feature", "properties": { "Id": 14, "Nombre": "CCH Vallejo", "Descripcion": null, "Nivel": "Bachillerato", "Poblacion": 11101 }, "geometry": { "type": "Point", "coordinates": [ -99.204553599999969, 19.4998328 ] } },
+{ "type": "Feature", "properties": { "Id": 15, "Nombre": "FES Acatlán", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 19898 }, "geometry": { "type": "Point", "coordinates": [ -99.222825194103876, 19.48893969 ] } },
+{ "type": "Feature", "properties": { "Id": 16, "Nombre": "FES Aragón", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 18122 }, "geometry": { "type": "Point", "coordinates": [ -99.045076, 19.4748125 ] } },
+{ "type": "Feature", "properties": { "Id": 17, "Nombre": "FES Zaragoza", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 12943 }, "geometry": { "type": "Point", "coordinates": [ -99.03760469662177, 19.38317454 ] } },
+{ "type": "Feature", "properties": { "Id": 18, "Nombre": "FES Iztacala", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 12655 }, "geometry": { "type": "Point", "coordinates": [ -99.186633015710981, 19.52573964 ] } },
+{ "type": "Feature", "properties": { "Id": 19, "Nombre": "FES Cuautitlán", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 15254 }, "geometry": { "type": "Point", "coordinates": [ -99.189971318627173, 19.69182477 ] } },
+{ "type": "Feature", "properties": { "Id": 20, "Nombre": "Ciudad Universitaria", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 103167 }, "geometry": { "type": "Point", "coordinates": [ -99.187023348433314, 19.33201904 ] } },
+{ "type": "Feature", "properties": { "Id": 21, "Nombre": "Facultad de Enfermería", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 2471 }, "geometry": { "type": "Point", "coordinates": [ -99.151408551358656, 19.29187003 ] } },
+{ "type": "Feature", "properties": { "Id": 22, "Nombre": "Facultad deArtes y Diseño", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 3410 }, "geometry": { "type": "Point", "coordinates": [ -99.118177797808443, 19.25208751 ] } },
+{ "type": "Feature", "properties": { "Id": 23, "Nombre": "Facultad de Música", "Descripcion": null, "Nivel": "Licenciatura", "Poblacion": 610 }, "geometry": { "type": "Point", "coordinates": [ -99.153764284879742, 19.35421932 ] } }
+]
+}
+
+export default escuelas;
